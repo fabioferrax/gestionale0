89836b0b9e75902f10be40d3a3f78c865e0c2fd2
GiulianoPes <giulianopes@gmail.com>
2016-11-21 16:45:38 +0100

    Prova

--- src/model/Lavoro.java
+++ src/model/Lavoro.java
@@ -1,12 +1,10 @@
 package model;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
 @Entity
 public class Lavoro {
